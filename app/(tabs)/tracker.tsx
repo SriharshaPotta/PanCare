f1240ae3fd6ef9255238cf4c89c812b5906243fd
Sriharsha Potta <sriharshapotta7@gmail.com>
2024-11-29 08:50:17 -0600

    Created tracker.tsx

---
+++ app/(tabs)/tracker.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const tracker = () => {
+  return (
+    <View>
+      <Text>tracker</Text>
+    </View>
+  )
+}
+
+export default tracker
